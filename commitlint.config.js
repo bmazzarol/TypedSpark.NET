222117896694fa093ec97f9f51a31694e912771c
Ben Mazzarol <bmazzarol@gmail.com>
2023-04-09 22:43:05 +0800

    feat: add standard files [ci]

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        "header-case": [1, "always", "sentence-case"],
+        "header-max-length": [2, "always", 100],
+        "scope-enum": _ => [
+            2,
+            "always",
+            [
+                "deps",
+                "docs"
+            ]
+        ]
+    }
+}
