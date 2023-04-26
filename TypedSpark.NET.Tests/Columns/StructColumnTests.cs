09d1294598a0121d19f065b4d51615dc83506f53
Ben Mazzarol <bmazzarol@gmail.com>
2023-04-26 22:43:43 +0800

    feat: complete struct column [ci]

---
+++ TypedSpark.NET.Tests/Columns/StructColumnTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading.Tasks;
+using BunsenBurner;
+using BunsenBurner.Verify.Xunit;
+using SparkTest.NET.Extensions;
+using TypedSpark.NET.Columns;
+using VerifyXunit;
+using Xunit;
+using static TypedSpark.NET.Tests.SparkTestExtensions;
+
+namespace TypedSpark.NET.Tests.Columns
+{
+    [UsesVerify]
+    public static class StructColumnTests
+    {
+        sealed class Schema1 : TypedSchema<Schema1>
+        {
+            public StringColumn A { get; private set; } = string.Empty;
+            public IntegerColumn B { get; private set; } = default(int);
+            public DateColumn C { get; private set; } = DateTime.Now;
+
+            public Schema1(string? alias)
+                : base(alias) { }
+
+            public Schema1()
+                : base(default) { }
+        }
+
+        sealed class Schema2 : TypedSchema<Schema1>
+        {
+            public DateColumn C { get; private set; } = DateTime.Now;
+            public LongColumn D { get; private set; } = default(int);
+
+            public Schema2(string? alias)
+                : base(alias) { }
+
+            public Schema2()
+                : base(default) { }
+        }
+
+        [Fact(DisplayName = "Struct column can be migrated")]
+        public static async Task Case1() =>
+            await ArrangeUsingSpark(s =>
+                {
+                    var col = StructColumn.New<Schema1>("test");
+                    return s.CreateDataFrameFromData(
+                            new
+                            {
+                                D = 100L,
+                                test = new
+                                {
+                                    A = "a",
+                                    B = 1,
+                                    C = DateTime.MinValue
+                                }
+                            }
+                        )
+                        .Select(col, col.Migrate<Schema2>());
+                })
+                .Act(df => df.Debug())
+                .AssertResultIsUnchanged();
+
+        [Fact(DisplayName = "Struct column can be created from columns")]
+        public static async Task Case2() =>
+            await ArrangeUsingSpark(
+                    s =>
+                        s.CreateDataFrameFromData(
+                                new
+                                {
+                                    A = "a",
+                                    B = 1,
+                                    C = DateTime.MinValue,
+                                    D = 100L
+                                }
+                            )
+                            .Select(
+                                StringColumn.New("A"),
+                                IntegerColumn.New("B"),
+                                DateColumn.New("C"),
+                                LongColumn.New("D"),
+                                StructColumn.FromColumns<Schema1>(),
+                                StructColumn.FromColumns<Schema2>()
+                            )
+                )
+                .Act(df => df.Debug())
+                .AssertResultIsUnchanged();
+    }
+}
--- TypedSpark.NET/Columns/StructColumn.cs
+++ TypedSpark.NET/Columns/StructColumn.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.Spark.Sql;
 using Microsoft.Spark.Sql.Types;
@@ -28,6 +29,28 @@ public sealed class StructColumn<T> : TypedColumn<StructColumn<T>, StructType>
     public TA Get<TA>(Expression<Func<T, TA>> getterExp)
         where TA : TypedColumn, new() =>
         new() { Column = Column.GetField(getterExp.GetPropertyInfo().Name) };
+
+    /// <summary>
+    /// Migrates a struct column to a new schema
+    /// </summary>
+    /// <typeparam name="TB">new schema</typeparam>
+    /// <returns>column</returns>
+    public StructColumn<TB> Migrate<TB>()
+        where TB : TypedSchema, new()
+    {
+        var newCol = new StructColumn<TB>();
+        var existingColNames = ColumnType.Fields.ConvertAll(x => x.Name);
+        var newColNames = newCol.ColumnType.Fields.ConvertAll(x => x.Name);
+        var toAdd = newColNames.Except(existingColNames, StringComparer.Ordinal).ToList();
+        var toRemove = existingColNames.Except(newColNames, StringComparer.Ordinal).ToArray();
+        newCol.Column = Column;
+        newCol.Column =
+            toAdd.Count > 0
+                ? toAdd.Aggregate(newCol.Column, (column, s) => column.WithField(s, F.Col(s)))
+                : newCol.Column;
+        newCol.Column = toRemove.Length > 0 ? newCol.Column.DropFields(toRemove) : newCol.Column;
+        return newCol;
+    }
 }
 
 public static class StructColumn
@@ -48,4 +71,17 @@ public static class StructColumn
     /// <returns>column</returns>
     public static StructColumn<T> New<T>(Column column)
         where T : TypedSchema, new() => new(column);
+
+    /// <summary>
+    /// Builds a new struct type from existing columns that match the schema
+    /// </summary>
+    /// <typeparam name="T">schema type</typeparam>
+    /// <returns>column</returns>
+    public static StructColumn<T> FromColumns<T>()
+        where T : TypedSchema, new()
+    {
+        var col = new StructColumn<T>();
+        col.Column = F.Struct(col.ColumnType.Fields.ConvertAll(x => F.Col(x.Name)).ToArray());
+        return col;
+    }
 }
